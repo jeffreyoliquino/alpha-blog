e929e586b87b839f7085586389e9bd01f280a442
Jeffrey Oliquino <joliquino@gmail.com>
2022-03-26 01:41:49 +0800

    add before_action in controller and create form and messages partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
+    
     end
 
     def new
@@ -14,11 +15,11 @@ class ArticlesController < ApplicationController
 
 
     def edit
-        @article = Article.find(params[:id])
+   
     end
 
     def create
-        @article = Article.new(params_article)
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created successfully"
             redirect_to @article
@@ -28,8 +29,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params_article)
+        if @article.update(article_params)
             flash[:notice] = "Article was edited successfully"
             redirect_to @article
         else
@@ -38,14 +38,17 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path, status: :see_other
     end
 
     private
 
-    def params_article
+    def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
     end
 end
